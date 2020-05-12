17117cea7d8b6e11e0df9700f18934080d31840d
garrr777 <58696830+garrr777@users.noreply.github.com>
2020-05-12 10:37:01 +0400

    davaleba

---
+++ davaleba1(1).js
@@ -0,0 +1,10 @@
+let i = 1;
+let sum = 0;
+
+while(i < 10) {
+    if (i % 2 != 0) {
+    sum = sum + i;
+    }
+    i++;
+}
+console.log(sum);
---
+++ davaleba1(2).js
@@ -0,0 +1,11 @@
+let sum = 0;
+let i = 0;
+
+while(i < 50){
+    if(i % 3 == 0){
+        sum += i;
+    }
+    i++;
+}
+
+console.log(sum);
